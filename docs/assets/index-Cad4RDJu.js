680a1e8642bdfa631acd2002d59884c4442f7a43
imzbf <37092006+imzbf@users.noreply.github.com>
2024-04-22 07:45:29 +0000

    Deploying to docs from @ imzbf/md-editor-rt@1087d2f2f45665cad3eb723896ed0d3a949654cb 🚀

--- docs/assets/index-Cad4RDJu.js
+++ docs/assets/index-Cad4RDJu.js
@@ -1,4 +1,4 @@
-import{u as l,a as t,j as n,I as c,h as p,k as u}from"./index-X9uIEYcM.js";const r=`## 😁 Basic Usage
+import{u as l,a as t,j as n,I as c,h as p,k as u}from"./index-C8FLCzOJ.js";const r=`## 😁 Basic Usage
 
 It has been developing iteratively, so update the latest version please. Publish logs: [releases](https://github.com/imzbf/md-editor-rt/releases)
 
